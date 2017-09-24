db44b6fa73212fa430b535bac68f18bd34a415db
Carrington Simecheck <c.simecheck89@yahoo.com>
2017-09-24 11:26:02 -0500

    created array for buttonss

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
+let action = function(showElement) {
+  let showAction = showElement;
+  function setMessage(showElement) {
+    showAction.innerHTML = showElement;
+    return showAction;
+  }
+  return (setMessage);
+};
+
 function main() {
   let board = document.querySelectorAll('button');
-  console.log(board);
+  let players = ['X', 'O'];
+  let currentTurn = 0;
+  let gameComplete = false;
+  let showAction = action(document.querySelector('#actions'));
+
+  showAction(`Game board is set. Player ${players[currentTurn]}, begin!`)
+  for (let i = 0; i < board.length; i++) {}
+
+
+
+
+
+
+
+
+
 }
+
 main();
+
