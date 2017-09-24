3db63bc8a6e8e6d7ba423ce65be08e730d432704
Carrington Simecheck <c.simecheck89@yahoo.com>
2017-09-24 12:43:29 -0500

    minor changes to improve functionality

--- app.js
+++ app.js
@@ -4,19 +4,10 @@ let action = function(showElement) {
     showAction.innerHTML = showElement;
     return showAction;
   }
-  return (setMessage);
+  return setMessage;
 };
 
-function valid(button) {
-  return button.innerHTML.length == 0;
-}
-
 function checkForWin(board, players, currentTurn) {
-  if (board[0].innerHTML === players[currentTurn] &&
-      board[0].innerHTML === players[currentTurn] &&
-      board[0].innerHTML === players[currentTurn]){
-    return true;
-  }
   if (board[0].innerHTML === players[currentTurn] &&
       board[1].innerHTML === players[currentTurn] &&
       board[2].innerHTML === players[currentTurn]){
@@ -57,7 +48,25 @@ function checkForWin(board, players, currentTurn) {
       board[8].innerHTML === players[currentTurn]){
     return true;
   }
-}
+  return false;
+};
+
+function full(board) {
+  for (let i = 0; i < board.length; i++) {
+    if (board[i].innerHTML.length == 0); {
+      return false;
+    }
+  }
+  return true;
+};
+
+function aDraw(board) {
+  return (!checkForWin(board, ["X"], 0)) && full(board);
+};
+
+function valid(button) {
+  return button.innerHTML.length == 0;
+};
 
 function setMarker(button, mark) {
   button.innerHTML = mark;
@@ -78,23 +87,23 @@ function main() {
         return;
       }
 
-      //write this
       if (!valid(this)) {
         showAction(`Invalid move, Player ${players[currentTurn]}!`)
       }
       else {
         setMarker(this, players[currentTurn]);
 
-        //Complete function gameComplete
         gameComplete = checkForWin(board, players, currentTurn);
 
-        if (checkForWin) {
-          showAction(`Player ${players[currentTurn]} has defeated their opponent`);
+        if (gameComplete) {
+          showAction(`Player ${players[currentTurn]} has defeated their opponent!`);
+          return;
         }
 
-        //over
-        //draw
-        //not over continue play
+        if (full(board)) {
+          showAction("Players X and O have died.");
+          return;
+        }
         currentTurn++;
 
         //Ensures current player is always X or O
