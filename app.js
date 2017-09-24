d10a428d34624adf7d273a37bfb42037deab00df
Carrington Simecheck <c.simecheck89@yahoo.com>
2017-09-24 11:39:29 -0500

    added player rotation to js

--- app.js
+++ app.js
@@ -12,19 +12,37 @@ function main() {
   let players = ['X', 'O'];
   let currentTurn = 0;
   let gameComplete = false;
-  let showAction = action(document.querySelector('#actions'));
+  let showAction = new action(document.querySelector('#actions'));
 
-  showAction(`Game board is set. Player ${players[currentTurn]}, begin!`)
-  for (let i = 0; i < board.length; i++) {}
+  showAction(`Game board is set. Player ${players[currentTurn]}, begin!`);
 
+  for (let i = 0; i < board.length; i++) {
+    board[i].addEventListener("click", function() {
+      if (gameComplete) {
+        return;
+      }
 
+      //write this
+      if (!valid(this)) {
+      }
+      else {
+        setMarker(this, players[currentTurn]);
 
+        //Complete function gameComplete
+        gameComplete = checkForWin(board, players, currentTurn);
 
+        //over
+        //draw
+        //not over continue play
+        currentTurn++;
 
+        //Ensures current player is always X or O
+        currentTurn = currentTurn % 2;
 
-
-
-
+        showAction(`It is ${currentTurn}\'s turn. Continue the battle!`);
+      }
+    })
+  }
 }
 
 main();
